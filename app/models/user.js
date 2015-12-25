f791226e6aeec6c4ab7f211c7a02e530d480632a
Ian Macalinao <me@ian.pw>
2015-12-24 18:34:38 -0600

    feat: add Sequelize database connection

---
+++ app/models/user.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+const database = require('../../lib/database')
+
+module.exports = database.define('user', {
+
+  email: {
+    type: Sequelize.STRING
+  },
+
+  password: {
+    type: Sequelize.STRING
+  },
+
+  confirmationToken: {
+    type: Sequelize.STRING,
+    field: 'confirmation_token'
+  },
+
+  confirmedAt: {
+    type: Sequelize.DATE,
+    field: 'confirmed_at'
+  },
+
+  createdAt: {
+    type: Sequelize.DATE,
+    field: 'created_at'
+  },
+
+  profile: {
+    type: Sequelize.JSONB
+  }
+
+})
