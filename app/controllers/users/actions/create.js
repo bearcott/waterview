588e041a322e9b9bed5c53c8ea6df63e82d382a1
Ian Macalinao <me@ian.pw>
2015-12-26 15:56:54 -0500

    feat: Validate create route

--- app/controllers/users/actions/create.js
+++ app/controllers/users/actions/create.js
@@ -1,7 +1,16 @@
 const co = require('co')
+const validate = require('koa-joi-schema')
+const Joi = validate.Joi
 const User = require('../../../models/user')
 
-module.exports = co.wrap(function *(ctx) {
-  const user = yield User.create(ctx.req.body)
+const validator = validate('request.body')(Joi.object().keys({
+  email: Joi.string().email().required(),
+  profile: Joi.object().required()
+}))
+
+const create = co.wrap(function *(ctx) {
+  const user = yield User.create(ctx.request.body)
   ctx.body = user
 })
+
+module.exports = compose([validator, create])
