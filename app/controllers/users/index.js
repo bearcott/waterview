91fbf95e6f4998b59271bd94f920ab2dcb526c23
Ian Macalinao <me@ian.pw>
2015-12-27 00:57:09 -0500

    feat: Export confirm and register routes

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -1,5 +1,7 @@
 const actions = {
+  confirm: require('./actions/confirm'),
   create: require('./actions/create'),
+  register: require('./actions/register'),
   show: require('./actions/show'),
   update: require('./actions/update')
 }
@@ -12,10 +14,14 @@ const Router = require('koa-router')
 
 const routes = router => {
   const users = new Router()
+
   users.post('/', actions.create)
   users.get('/:id', actions.show)
   users.put('/:id', actions.update)
   users.patch('/:id', actions.update)
+  users.get('/confirm', actions.confirm)
+  users.post('/register', actions.register)
+
   router.use('/users', users.routes(), users.allowedMethods())
 }
 
