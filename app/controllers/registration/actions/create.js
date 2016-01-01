e9b527d28a251b3bbb0a65442cb31e2dede66d02
Ian Macalinao <me@ian.pw>
2015-12-31 23:35:40 -0500

    feat: use sendgrid email template

--- app/controllers/registration/actions/create.js
+++ app/controllers/registration/actions/create.js
@@ -6,6 +6,28 @@ const emails = require('../../../../lib/util/emails')
 const Joi = require('joi')
 const User = require('../../../models/user')
 
+function buildLink(email, token) {
+  return `http://app.hackdfw.com/registration/complete?email=${email}&token=${token}`
+}
+
+function sendConfirmationEmail(email, token) {
+  return emails.sendTemplate('af2de2ce-ce8e-4b0d-a420-ffa736b86bd0', {
+    to: email,
+    from: 'confirm',
+    fromname: 'HackDFW Team',
+    subject: '[ACTION REQUIRED] Confirm your registration for HackDFW',
+    text: 'Hey there!\n\n'
+      + 'Thanks for registering for HackDFW. To complete your registration, please follow the link below:\n\n'
+      + `${buildLink(email, token)}\n\n`
+      + 'Regards,\n\nThe HackDFW Team',
+    html: 'Hey there!<br /><br />'
+      + 'Thanks for registering for HackDFW. To complete your registration, please follow the link below:<br /><br />'
+      + `<a href="${buildLink(email, token)}">${buildLink(email, token)}</a><br /><br />`
+      + 'Regards,<br /><br />The HackDFW Team',
+  })
+
+}
+
 const validator = validate('request.body')(Joi.object().keys({
   email: Joi.string().email().required(),
   profile: Joi.object().keys({
@@ -15,7 +37,7 @@ const validator = validate('request.body')(Joi.object().keys({
 
 const create = co.wrap(function *(ctx) {
   const body = ctx.request.body
-  body.confirmationToken = randomstring.generate()
+  const token = body.confirmationToken = randomstring.generate()
 
   try {
     const user = yield User.create(body)
@@ -28,16 +50,7 @@ const create = co.wrap(function *(ctx) {
     return
   }
 
-  yield emails.sendEmail({
-    to: user.email,
-    from: 'confirm',
-    fromname: 'HackDFW Team',
-    subject: '[ACTION REQUIRED] Confirm your registration for HackDFW',
-    text: 'Hey there!\n\n'
-      + 'Thanks for registering for HackDFW. To complete your registration, please follow the link below:\n\n'
-      + '<register link>\n\n'
-      + 'Regards,\n\nThe HackDFW Team'
-  })
+  yield sendConfirmationEmail(user.email, token)
 
   ctx.body = {
     data: user
