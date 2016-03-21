7a3002e93d6f4648b3131bedb536eac8ab2f3f9b
bearcott <bear@bearcott.com>
2016-03-21 12:22:56 -0500

    fix: get profile working

--- app/controllers/auth/actions/get_profile.js
+++ app/controllers/auth/actions/get_profile.js
@@ -1,13 +1,22 @@
 const compose = require('koa-compose')
 const jwt = require('koa-jwt')
+const co = require('co')
 
 const constants = require('../../../../lib/constants')
+const User = require('../../../models/user')
 const ensureAuthenticated = require('../../../../lib/helpers/ensure_authenticated')
 
-const get_profile = ctx => {
+const get_profile = co.wrap(function*(ctx) {
+  const email = ctx.state.user.email;
+
+  //maybe I need to handle this.. but state cant be invalid so...
+  const user = yield User.findOne({
+    where: { email: email }
+  })
+
   ctx.body = {
-    profile: ctx.state.user.profile
+    profile: user.profile
   }
-}
+});
 
 module.exports = compose([ensureAuthenticated, get_profile])
